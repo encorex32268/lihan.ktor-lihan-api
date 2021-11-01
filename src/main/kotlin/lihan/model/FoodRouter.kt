92552b2cb14718e85a76882bcb92e508bf01c420
encorex32268 <encorex32268@gmail.com>
2021-11-01 22:19:09 +0900

    Food api done

--- src/main/kotlin/lihan/model/FoodRouter.kt
+++ src/main/kotlin/lihan/model/FoodRouter.kt
@@ -1,18 +1,18 @@
 package lihan.model
 
-import com.example.food.Food
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-const val BASE_URL = "http://192.168.0.240:8080"
+const val BASE_URL = "http://cf6c-160-237-147-77.ngrok.io"
 val foods = arrayListOf<Food>(
-    Food("牛肉麵",110,"台北市大安區敦化南路1段151號1樓","$BASE_URL/food/food1.png"),
-    Food("滷肉飯",10,"台北市大同區延平北路二段 No. 233 號","$BASE_URL/food/food2.png"),
-    Food("蚵仔煎",65,"台北市大同區寧夏路46號","$BASE_URL/food/food3.png"),
-    Food("蚵仔麵線",40,"台北市萬華區大理街93號1樓","$BASE_URL/food/food4.png"),
-    Food("牛排",150,"台灣台北市士林區大南路85號","$BASE_URL/food/food5.png")
+    //http://localhost:8080/static/food/food5.png
+    Food("牛肉麵",110,"台北市大安區敦化南路1段151號1樓","$BASE_URL/static/food/food1.png"),
+    Food("滷肉飯",10,"台北市大同區延平北路二段 No. 233 號","$BASE_URL/static/food/food2.png"),
+    Food("蚵仔煎",65,"台北市大同區寧夏路46號","$BASE_URL/static/food/food3.png"),
+    Food("蚵仔麵線",40,"台北市萬華區大理街93號1樓","$BASE_URL/static/food/food4.png"),
+    Food("牛排",150,"台灣台北市士林區大南路85號","$BASE_URL/static/food/food5.png")
 )
 
 
